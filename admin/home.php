57dc7cb286e2f33653527915c9a9a1a808aebf6a
alwasata <email@example.com>
2021-05-23 21:28:42 +0200

    email templ finished

--- admin/home.php
+++ admin/home.php
@@ -9,7 +9,7 @@ check_login();
 <head>
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <meta charset="utf-8" />
-<title>CRM | Admin Dashboard</title>
+<title>نظام ادارة دعم العملاء</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <meta content="" name="description" />
 <meta content="" name="author" />
@@ -37,7 +37,10 @@ check_login();
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>     
 </head>
 <body class="">
-<?php include("header.php");?>
+<?php include("header.php");if(isset($_GET['mesg']))
+{
+	echo "<script>alert('لقد تم ارسال البريد الإلكتروني بنجاح .');</script>";
+}?>
 <div class="page-container row"> 
   
       <?php include("leftbar.php");?>
